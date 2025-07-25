0e84d1c7ae6657dc46e482c10b2ccac46c3340a4
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-07-25 12:39:54 -0300

    add endpoint to specific book

--- src/routes/data.routes.ts
+++ src/routes/data.routes.ts
@@ -11,11 +11,32 @@ export async function dataRoutes(app: FastifyInstance) {
         } catch (err) {
             console.log(err);
 
-            return reply
-                .status(500)
-                .send({
-                    error: "Erro interno no servidor.",
-                });
+            return reply.status(500).send({
+                error: "Erro interno no servidor.",
+            });
+        }
+    });
+
+    app.get("/:abrev", async (request, reply) => {
+        try {
+            const { abrev } = request.params as {
+                abrev: string;
+            };
+            const book = await dataService.getBook(abrev);
+
+            if (!book) {
+                throw new Error("Livro não encontrado");
+            }
+
+            return reply.status(200).send({
+                data: book,
+            });
+        } catch (err) {
+            console.log(err);
+
+            return reply.status(400).send({
+                error: "Livro não encontrado",
+            });
         }
     });
 }
--- src/services/data.services.ts
+++ src/services/data.services.ts
@@ -17,4 +17,20 @@ export class DataService {
             throw new Error("Failed to load data");
         }
     }
+
+    async getBook(
+        abrev: string
+    ): Promise<Book | undefined> {
+        try {
+            this.data = await this.loadData();
+
+            return this.data.find(
+                (book) => book.abrev === abrev
+            );
+        } catch (err) {
+            console.error(err);
+
+            throw new Error("Erro ao procurar livro");
+        }
+    }
 }
